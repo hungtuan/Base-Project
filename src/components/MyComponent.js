801462bcb0910e5b43ace47dd6c48796ef1e76db
hungtuan <hungtuan2072000@gmail.com>
2022-12-13 18:02:58 +0700

    video 16

---
+++ src/components/MyComponent.js
@@ -0,0 +1,15 @@
+//class Component
+//funtion Component
+import React from "react";
+class MyComponent extends React.Component {
+  //JSX
+  render() {
+    return (
+      <div>
+        my firt component sadadsa
+        {Math.random()}
+      </div>
+    );
+  }
+}
+export default MyComponent;
